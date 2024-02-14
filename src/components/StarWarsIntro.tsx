3d3e3feb457af064f9e8f1b9a67bb261428a1448
AitorMelero <meleropiconaitor@gmail.com>
2024-02-14 20:35:40 +0100

    Feat: Transition effect from prelude to crawl text

--- src/components/StarWarsIntro.tsx
+++ src/components/StarWarsIntro.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from 'react'
 import { Audio } from './Audio'
 import { CrawlText } from './CrawlText'
 import { Prelude } from './Prelude'
 
 export const StarWarsIntro: React.FC = () => {
+  const [isPrelude, setIsPrelude] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsPrelude(false)
+    }, 9000)
+  }, [])
+
   return (
     <>
-      {/* <Audio /> */}
-      <Prelude />
-      {/* <CrawlText /> */}
+      <Audio />
+      {isPrelude ? <Prelude /> : <CrawlText />}
     </>
   )
 }
