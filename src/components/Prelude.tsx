bd516a655de543d0efa5b1b0f585a6676b1dafbd
AitorMelero <meleropiconaitor@gmail.com>
2024-02-29 14:22:21 +0100

    feat: Add responsive design in Prelude component

--- src/components/Prelude.tsx
+++ src/components/Prelude.tsx
@@ -1,12 +1,15 @@
 import '../styles/Prelude.css'
 
 export const Prelude: React.FC = () => {
+  const line1 = 'A long time ago in a galaxy far,'
+  const line2 = 'far away....'
+
   return (
     <section className="prelude">
-      <article className="toFadeInAndOutAnimation">
-        A long time ago in a galaxy far,
+      <article className="prelude-container toFadeInAndOutAnimation">
+        {line1}
         <br />
-        far away....
+        {line2}
       </article>
     </section>
   )
