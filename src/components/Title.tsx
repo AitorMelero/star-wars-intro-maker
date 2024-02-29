922744188bd0a861660859d7ca27127ac6a3949d
AitorMelero <meleropiconaitor@gmail.com>
2024-02-29 14:41:36 +0100

    Feat: Convert text title in two text lines

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,15 @@
 import '../styles/Title.css'
 
 export const Title: React.FC = () => {
+  const line1 = 'Star'
+  const line2 = 'Wars'
+
   return (
     <header className="star-wars-title-header">
       <h1 className="star-wars-title">
-        Star <br />
-        Wars
+        {line1}
+        <br />
+        {line2}
       </h1>
     </header>
   )
