f7583e1f6093f2b3abbd6d83eddb1a80b254fb08
AitorMelero <meleropiconaitor@gmail.com>
2024-03-11 10:47:13 +0100

    feat: Add play intro button style

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,9 +1,13 @@
+import '../styles/PlayButton.css'
+
 interface Props {
   playIntro: () => void
 }
 
 export const PlayButton: React.FC<Props> = ({ playIntro }) => {
   return (
-    <button onClick={playIntro}>Play</button>
+    <div className="play-button-container">
+      <button onClick={playIntro}>Play</button>
+    </div>
   )
 }
