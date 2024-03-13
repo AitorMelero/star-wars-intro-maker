96e9a2eecfee0d27616c8cfc79d926686f6ab1e7
AitorMelero <meleropiconaitor@gmail.com>
2024-03-13 09:27:12 +0100

    feat: Add Zod max line number validation

--- src/schemas/form.ts
+++ src/schemas/form.ts
@@ -1,26 +1,20 @@
 import { z } from 'zod'
 import { type FormType } from '../types/Form'
 
-// console.log(data.title.split(/\r\n|\r|\n/).length)
-
-// Max line characters: columns
-// const normalize = (text: string): string => text.replaceAll('\r\n', '\n')
-// title: z.string().transform(normalize).pipe(z.string().max(2, { message: 'This field text can\'t have more than 2 lines.' }))
-
-const normalize = (text: string): string => text.replaceAll('\r\n', '\n')
-// const normalize = (text: string): string[] => text.split(/\r\n|\r|\n/)
-// const normalize = (text: string): number => text.split(/\r\n|\r|\n/).length
-
 const formSchema = z.object({
-  prelude: z.string().min(1, { message: 'This field is required' }),
-  title: z.string().min(1, { message: 'This field is required' })
+  prelude: z.string()
+    .min(1, { message: 'This field is required' })
+    .transform(text => text.split(/\r\n|\r|\n/).length).refine(
+      (val) => val <= 2,
+      { message: 'This field text can\'t have more than 2 lines.' }
+    ),
+  title: z.string()
+    .min(1, { message: 'This field is required' })
+    .transform(text => text.split(/\r\n|\r|\n/).length).refine(
+      (val) => val <= 2,
+      { message: 'This field text can\'t have more than 2 lines.' }
+    )
 })
-  .and(
-    z.object({
-      title: z.string().transform(normalize).pipe(z.string().max(2, { message: 'This field text can\'t have more than 2 lines.' }))
-      // title: z.string().transform((text) => text.split(/\r\n|\r|\n/).length).pipe(z.number().max(2, { message: 'This field text can\'t have more than 2 lines.' }))
-    })
-  )
 
 interface ErrorValidate {
   path: string
