2941531f0e03a276b6ae6f4bca33ca5ab1c1a811
AitorMelero <meleropiconaitor@gmail.com>
2024-03-14 09:52:53 +0100

    fix: Fix translate to init position when reload intro play page

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -6,14 +6,23 @@ import { Title } from '../components/Title'
 import { useIntro } from '../hooks'
 import { type IntroType } from '../types/Intro'
 import '../styles/StarWarsIntro.css'
+import { useEffect } from 'react'
 
 export const Intro: React.FC = () => {
   const { data, isPlay } = useLoaderData() as IntroType
   const { prelude, title, episode, episodeTitle, crawlText } = data
   const { isPlaying, isPrelude, isTitle, playIntro } = useIntro(isPlay)
 
+  useEffect(() => {
+    const mainIntroElement = document.getElementById('main-intro')
+
+    if (mainIntroElement !== null) {
+      mainIntroElement.style.transform = 'translate(0, 0)'
+    }
+  }, [])
+
   return (
-    <main className={`star-wars-intro-background ${isPlaying ? 'star-wars-intro-background-animation' : ''} `}>
+    <main id='main-intro' className={`star-wars-intro-background ${isPlaying ? 'star-wars-intro-background-animation' : ''} `}>
       <section className="star-background-1">
         {!isPlaying
           ? <PlayButton playIntro={playIntro} />
