b078699fe31c4e8fcba28cd13aafb7e6cdb8b615
AitorMelero <meleropiconaitor@gmail.com>
2024-03-18 13:49:01 +0100

    feat: Add Error page styles

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,5 @@
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import '../styles/ErrorPage.css'
 
 export const Error: React.FC = () => {
   const error = useRouteError()
@@ -9,14 +10,12 @@ export const Error: React.FC = () => {
   }
 
   return (
-    <>
-      <div>
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has ocurred.</p>
-        <p>
-          <i>{statusText}</i>
-        </p>
-      </div>
-    </>
+    <div className='error-page'>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has ocurred.</p>
+      <p>
+        <i>{statusText}</i>
+      </p>
+    </div>
   )
 }
